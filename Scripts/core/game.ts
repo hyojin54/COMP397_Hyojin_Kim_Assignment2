14fe6f1f5fc9ae16052062b19dc81fcbd70ddf8d
hyojin54 <38159782+hyojin54@users.noreply.github.com>
2018-07-29 15:45:54 -0400

    added abstract Scene class

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -4,20 +4,23 @@
     // Game Variables
     let canvas: HTMLCanvasElement;
     let stage: createjs.Stage;
-    let welcomeLabel:  objects.Label;
+    let welcomeLabel: objects.Label;
     let btnStart: objects.Button;
     let btnExit: objects.Button;
     let btnInstructions: objects.Button;
     let assetManager: createjs.LoadQueue;
 
     let Manifest = [
-        {id: "btnStart", src:"/Assets/images/button_start.jpg"},
-        {id: "btnExit", src:"/Assets/images/button_exit.jpg"},
-        {id: "btnInstructions", src:"/Assets/images/button_instructions.jpg"}
-    ]
+        { id: "btnStart", src: "/Assets/images/button_start.jpg" },
+        { id: "btnExit", src: "/Assets/images/button_exit.jpg" },
+        { id: "btnInstructions", src: "/Assets/images/button_instructions.jpg" }
+    ];
 
     function Init(): void {
-        console.log(`%c Assets Loading...`,"font-weight:bold; font-size:20px; color:green;");
+        console.log(
+            `%c Assets Loading...`,
+            "font-weight:bold; font-size:20px; color:green;"
+        );
         assetManager = new createjs.LoadQueue();
         managers.Game.AssetManager = assetManager;
         assetManager.installPlugin(createjs.Sound); // enables preloading of sound assets
@@ -26,7 +29,10 @@
     }
 
     function Start(): void {
-        console.log(`%c App Starting...`, "font-weight:bold; font-size:20px; color:red;");
+        console.log(
+            `%c App Starting...`,
+            "font-weight:bold; font-size:20px; color:red;"
+        );
         canvas = document.getElementsByTagName("canvas")[0];
         stage = new createjs.Stage(canvas);
         stage.enableMouseOver(20); // enables mouseover events
@@ -42,28 +48,38 @@
     }
 
     function Main(): void {
-        console.log(`%c Main Function`, "font-style:italic; font-size:16px; color:black;");
+        console.log(
+            `%c Main Function`,
+            "font-style:italic; font-size:16px; color:black;"
+        );
 
         // label
-        welcomeLabel = new objects.Label("Welcome", "60px", "Consolas", "#000000", 320, 50, true);
+        welcomeLabel = new objects.Label(
+            "Welcome",
+            "60px",
+            "Consolas",
+            "#000000",
+            320,
+            50,
+            true
+        );
         stage.addChild(welcomeLabel);
 
         // buttons
         btnStart = new objects.Button("btnStart", 320, 200, true);
         btnInstructions = new objects.Button("btnInstructions", 320, 300, true);
         btnExit = new objects.Button("btnExit", 320, 400, true);
-        
+
         stage.addChild(btnStart);
         stage.addChild(btnInstructions);
         stage.addChild(btnExit);
 
         // TODO
-        btnStart.on("click", function(){
+        btnStart.on("click", function () {
             console.log(`clicked`);
             //welcomeLabel.text = "Clicked!";
         });
     }
 
     window.addEventListener("load", Init);
-
 })();
